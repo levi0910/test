81c034e926506032e08f899d448526a513a3bec1
levi0910 <77359676+levi0910@users.noreply.github.com>
2021-02-21 16:04:21 -0500

    Delete Server directory

--- Server/mysql/actions/createDatabase.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql')
-const con = require('./connection')
-
-con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-
